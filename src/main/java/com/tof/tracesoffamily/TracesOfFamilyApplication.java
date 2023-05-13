36a534b15bd9739b19d8b16ffad13c13223a4d4a
zbass90 <smilejkh90@gmail.com>
2023-05-13 22:37:28 +0900

    Initial commit

---
+++ src/main/java/com/tof/tracesoffamily/TracesOfFamilyApplication.java
@@ -0,0 +1,13 @@
+package com.tof.tracesoffamily;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TracesOfFamilyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TracesOfFamilyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tof/tracesoffamily/TracesOfFamilyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tof.tracesoffamily;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TracesOfFamilyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
